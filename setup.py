b5f943d35419d3d468fdfcbb5d6f62c06d49f4ef
Reinoud van Leeuwen <reinoud@startmail.com>
2020-01-08 12:02:16 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='transip_rest_client',
+    version='0.1',
+    packages=['transip_rest_client', 'transip_rest_client.tests'],
+    url='',
+    license='',
+    author='Startmail.com',
+    author_email='root@startmail.com',
+    description='Python3 Rest client for TransIP API'
+)
