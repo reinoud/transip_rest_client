b7704b377c9479f5c268e4f8dd6892abd23ee12d
Jasper Spaans <jasper@startmail.com>
2020-01-16 17:00:12 +0100

    Inline setup.py variables, and add dependencies.

--- setup.py
+++ setup.py
@@ -16,16 +16,19 @@
 #
 
 from setuptools import setup
-from transip_rest_client.__version__ import __version__, __name__, __author__, __author_email__, __description__, \
-    __license__
 
 setup(
-    name=__name__,
-    version=__version__,
+    name='transip-rest-client',
     packages=['transip_rest_client'],
     url='',
-    license=__license__,
-    author=__author__,
-    author_email=__author_email__,
-    description=__description__
+    license='MIT',
+    author='StartMail.com BV',
+    author_email='root@startmail.com',
+    description='A Python3 implementation for the TransIP REST API',
+    setup_requires=['setuptools_scm'],
+    use_scm_version=True,
+    install_requires=[
+        'cryptography',
+        'requests',
+    ],
 )
